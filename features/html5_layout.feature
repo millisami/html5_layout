626dc9d1f070a640235da49adba96f08419e95de
Millisami <millisami@gmail.com>
2010-11-20 00:02:52 +0545

    added the sass option

--- features/html5_layout.feature
+++ features/html5_layout.feature
@@ -22,3 +22,19 @@ Feature: HTML5 Boilerplate Layout Generator
     Then I should see "stylesheet_link_tag "foo"" in file "app/views/layouts/foo.html.erb"
     And I should see "stylesheet_link_tag "handheld"" in file "app/views/layouts/foo.html.erb"
     And I should see file "public/stylesheets/foo.css"
+
+  Scenario: Generate normal application layout with the sass option
+    Given a new Rails app
+    When I run "rails g html5_layout -f --sass"
+    Then I should see "stylesheet_link_tag "application"" in file "app/views/layouts/application.html.erb"
+    And I should see "stylesheet_link_tag "handheld"" in file "app/views/layouts/application.html.erb"
+    # And I should see file "app/helpers/layout_helper.rb"
+    # And I should see file "app/helpers/error_messages_helper.rb"
+    And I should see file "app/stylesheets/application.sass"
+    And I should see file "app/stylesheets/handheld.sass"
+    And I should see file "config/compass.rb"
+    And I should see "gem "compass", ">=0.10.5"" in file "Gemfile"
+    And I should see "gem "compass-960-plugin", ">=0.10.0"" in file "Gemfile"
+    And I should see file "public/javascripts/dd_belatedpng.js"
+    And I should see file "public/javascripts/jquery-1.4.2.min.js"
+    And I should see file "public/javascripts/modernizr-1.5.min.js"
--- lib/generators/html5_layout/html5_layout_generator.rb
+++ lib/generators/html5_layout/html5_layout_generator.rb
@@ -7,15 +7,23 @@ class Html5LayoutGenerator < Rails::Generators::Base
 	
   argument :layout_name, :type => :string, :default => "application"
   class_option :stylesheet, :type => :boolean, :default => true, :desc => "Include stylesheet file"
+  class_option :sass, :type => :boolean, :default => false, :desc => "Include the SASS version for CSS"
 
   def generate_layout
-    copy_file "stylesheet.css", "public/stylesheets/#{file_name}.css" if options.stylesheet?
-    copy_file "handheld.css", "public/stylesheets/handheld.css" if options.stylesheet?
-    
+    if options.sass?
+      # copy_file "stylesheet.sass", "public/stylesheets/#{file_name}.css" if options.stylesheet?
+      directory 'app'
+      gem 'compass', '>=0.10.5'
+      gem 'compass-960-plugin', '>=0.10.0'
+      copy_file "compass.rb", "config/compass.rb"
+    else
+      copy_file "stylesheet.css", "public/stylesheets/#{file_name}.css" if options.stylesheet?
+      copy_file "handheld.css", "public/stylesheets/handheld.css" if options.stylesheet?
+    end
     copy_file "modernizr-1.5.min.js", "public/javascripts/modernizr-1.5.min.js"
     copy_file "jquery-1.4.2.min.js", "public/javascripts/jquery-1.4.2.min.js"
     copy_file "dd_belatedpng.js", "public/javascripts/dd_belatedpng.js"
-    
+
     template "layout.html.erb", "app/views/layouts/#{file_name}.html.erb"
   end
   
